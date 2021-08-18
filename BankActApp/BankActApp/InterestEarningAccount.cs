d6cb6d0836cb2e2ca07a0fd5d122589982d8eda8
abdullah-rehman786 <air711@live.com>
2021-08-18 15:56:13 -0500

    Finished BankActApp
    added different account types

---
+++ BankActApp/BankActApp/InterestEarningAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankActApp
+{
+    public class InterestEarningAccount : BankAccount
+    {
+        public InterestEarningAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance > 500m)
+            {
+                var interest = Balance * 0.05m;
+                MakeDeposit(interest, DateTime.Now, "apply monthly interest");
+            }
+        }
+    }
+}
---
+++ BankActApp/BankActApp/LineOfCreditAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankActApp
+{
+    class LineOfCreditAccount : BankAccount
+    {
+        public LineOfCreditAccount(string name, decimal initialBalance, decimal creditLimit) : base(name, initialBalance, -creditLimit)
+        {
+           
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance < 0)
+            {
+                // Negate the balance to get a positive interest charge:
+                var interest = -Balance * 0.07m;
+                MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+            }
+        }
+        // <AddOverdraftFee>
+        protected override Transaction CheckWithdrawalLimit(bool isOverdrawn)
+        {
+            return isOverdrawn ? new Transaction(-20, DateTime.Now, "Apply overdraft fee") : default;
+        }
+        // </AddOverdraftFee>
+    }
+}
